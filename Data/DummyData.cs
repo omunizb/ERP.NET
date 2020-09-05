046f0c4f215f83034859936c8271874d3c81baab
omunizb <oriolmuniz@gmail.com>
2020-09-05 23:34:00 +0200

    dummy data completed

--- Data/DummyData.cs
+++ Data/DummyData.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 // Adapted from https://github.com/medhatelmasry/HealthAPI/blob/master/HealthAPI/Data/DummyData.cs
 namespace ERPProject.Data
 {
-    /*
     public class DummyData
     {
         public static void Initialize(IApplicationBuilder app)
@@ -28,7 +27,7 @@ namespace ERPProject.Data
                 context.SaveChanges();
 
                 var employees = GetEmployees().ToArray();
-                context.Users.AddRange(employees);
+                context.Employees.AddRange(employees);
                 context.SaveChanges();
 
                 var customers = GetCustomers().ToArray();
@@ -48,7 +47,7 @@ namespace ERPProject.Data
                     Name="Earbuds 100X",
                     Category="Electronics",
                     Description = "The best earbuds in the market!",
-                    CurrentPrice = 17.99,
+                    CurrentPrice = 17.99F,
                     Stock = 4500,
                     Purchases = 102
                 },
@@ -56,7 +55,7 @@ namespace ERPProject.Data
                     Name="Blue shirt",
                     Category="Men's Shirts",
                     Description = "A top-quality cotton shirt, in blue",
-                    CurrentPrice = 20.49,
+                    CurrentPrice = 20.49F,
                     Stock = 1200,
                     Purchases = 323
                 },
@@ -79,61 +78,128 @@ namespace ERPProject.Data
                                   warning, and a tour de force of narrative suspense, The Handmaid’s 
                                   Tale is a modern classic. Includes an introduction by Margaret 
                                   Atwood. Paperback – March 16, 1998",
-                    CurrentPrice = 7.99,
+                    CurrentPrice = 7.99F,
                     Stock = 3004,
                     Purchases = 2809 }
             };
             return products;
         }
 
-        public static List<User> GetUsers()
+        public static List<Employee> GetEmployees()
         {
-            List<User> users = new List<User>() {
-                new User {
-                    Name = "Gerard",
-                    Surname = "Ferrer",
-                    Email = "gf@gmail.com",
-                    Type = "student",
-                    IdItinerary = 6
+            List<Employee> employees = new List<Employee>() {
+                new Employee {
+                    Name = "Mikaela",
+                    Surname = "Gomis",
+                    Hired = new DateTime(2018, 9, 15),
+                    Salary = 31560.56F,
+                    Position = "Attendant"
                 },
-                new User {
-                    Name = "Oriol",
-                    Surname = "Muñiz",
-                    Email = "om@gmail.com",
-                    Type = "student",
-                    IdItinerary = 6
+                new Employee {
+                    Name = "Kevin",
+                    Surname = "Garcia",
+                    Hired = new DateTime(2015, 5, 23),
+                    Salary = 31560.56F,
+                    Position = "Attendant"
                 },
-                new User {
-                    Name = "Francisco Javier",
-                    Surname = "Rivas",
-                    Email = "fjr@gmail.com",
-                    Type = "student",
-                    IdItinerary = 6
+                new Employee {
+                    Name = "Joan",
+                    Surname = "Cot",
+                    Hired = new DateTime(2019, 10, 5),
+                    Salary = 63810.13F,
+                    Position = "Manager"
                 }
             };
-            return users;
+            return employees;
         }
 
-        public static List<Attendee> GetAttendees()
+        public static List<Customer> GetCustomers()
         {
-            List<Attendee> attendees = new List<Attendee>() {
-                new Attendee {
-                    IdEvent = 1,
-                    IdStudent = 1
+            List<Customer> customers = new List<Customer>() {
+                new Customer {
+                    Name = "Nuria",
+                    Surname = "Rossi",
+                    Email = "nuriarossi@gmail.com",
+                    FirstPurchase = new DateTime(2014, 6, 1, 18, 17, 53),
+                    LatestPurchase = new DateTime(2020, 8, 28, 11, 45, 12),
+                    TotalExpenditure = 568.33F,
+                    TotalPurchases = 41,
+                    DeliveryAddress = "Calle Enxertos 85, Monsagro, Salamanca, 37532, Spain",
+                    BillingAddress = "Calle Enxertos 85, Monsagro, Salamanca, 37532, Spain",
+                    BankAccount = "ES9121000418450200051332",
                 },
-                new Attendee {
-                    IdEvent = 2,
-                    IdStudent = 2,
-                    Accepted = true
+                new Customer {
+                    Name = "Hector",
+                    Surname = "Roca",
+                    Email = "hectorroca@gmail.com",
+                    FirstPurchase = new DateTime(2013, 3, 23, 22, 42, 11),
+                    LatestPurchase = new DateTime(2020, 6, 11, 18, 10, 24),
+                    TotalExpenditure = 445.65F,
+                    TotalPurchases = 29,
+                    DeliveryAddress = "Calle Pascual Yunquera 40, Cortes de Baza, Granada, 18814, Spain",
+                    BillingAddress = "Calle Pascual Yunquera 40, Cortes de Baza, Granada, 18814, Spain",
+                    BankAccount = "ES4031907163513548452719",
                 },
-                new Attendee {
-                    IdEvent = 2,
-                    IdStudent = 3,
-                    Accepted = true
+                new Customer {
+                    Name = "Alex",
+                    Surname = "Perez",
+                    Email = "alexperez@gmail.com",
+                    FirstPurchase = new DateTime(2017, 1, 29, 9, 9, 55),
+                    LatestPurchase = new DateTime(2020, 9, 2, 20, 19, 1),
+                    TotalExpenditure = 2899.09F,
+                    TotalPurchases = 113,
+                    DeliveryAddress = "Calle Cuevas de Ambrosio 72, León, León, 24000, Spain",
+                    BillingAddress = "Calle Extramuros 11, Valdemoro, Madrid, 28340, Spain",
+                    BankAccount = "ES7701824756598176787587",
                 },
             };
-            return attendees;
+            return customers;
+        }
+
+        public static List<Order> GetOrders()
+        {
+            List<Order> orders = new List<Order>() {
+                new Order {
+                    IdCustomer = 1,
+                    IdEmployee = 1,
+                    IdProduct = 1,
+                    Time = new DateTime(2015, 2, 27, 19, 18, 54),
+                    Quantity = 1,
+                    Price = 14.87F,
+                    PriceVAT = 17.99F,
+                    State = "Delivered",
+                    Priority = 3,
+                    ExpectedDelivery = new DateTime(2015, 3, 2),
+                    Delivered = new DateTime(2015, 3, 2)
+                },
+                new Order {
+                    IdCustomer = 2,
+                    IdEmployee = 2,
+                    IdProduct = 2,
+                    Time = new DateTime(2020, 6, 11, 18, 10, 24),
+                    Quantity = 1,
+                    Price = 16.93F,
+                    PriceVAT = 20.49F,
+                    State = "Out for delivery",
+                    Priority = 1,
+                    ExpectedDelivery = new DateTime(2020, 6, 12)
+                },
+                new Order {
+                    IdCustomer = 3,
+                    IdEmployee = 3,
+                    IdProduct = 3,
+                    Time = new DateTime(2017, 1, 29, 9, 9, 55),
+                    Devolution = true,
+                    Quantity = 1,
+                    Price = 6.60F,
+                    PriceVAT = 7.99F,
+                    State = "Delivered",
+                    Priority = 5,
+                    ExpectedDelivery = new DateTime(2017, 2, 2),
+                    Delivered = new DateTime(2017, 2, 6)
+                },
+            };
+            return orders;
         }
     }
-    */
 }
