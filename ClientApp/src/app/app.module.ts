7671802f3c6fc82b7321ba6732cf4045ff0222de
omunizb <oriolmuniz@gmail.com>
2020-09-26 20:42:26 +0200

    generated all necessary components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StockComponent } from './stock/stock.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { LoginComponent } from './login/login.component';
+import { CostumersComponent } from './costumers/costumers.component';
+import { OrdersComponent } from './orders/orders.component';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StockComponent
+    StockComponent,
+    EmployeesComponent,
+    LoginComponent,
+    CostumersComponent,
+    OrdersComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule
---
+++ ClientApp/src/app/costumers/costumers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-costumers',
+  templateUrl: './costumers.component.html',
+  styleUrls: ['./costumers.component.css']
+})
+export class CostumersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
