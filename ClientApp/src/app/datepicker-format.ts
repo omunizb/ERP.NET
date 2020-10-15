16f7ec013ed80708af0d19ac63b501dbf0f19265
omunizb <oriolmuniz@gmail.com>
2020-10-15 16:45:49 +0200

    Angular: stats component final design

--- ClientApp/src/app/datepicker-format.ts
+++ ClientApp/src/app/datepicker-format.ts
@@ -1,6 +1,8 @@
 import { NativeDateAdapter } from '@angular/material/core';
 import { MatDateFormats } from '@angular/material/core';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AppDateAdapter extends NativeDateAdapter {
   format(date: Date, displayFormat: Object): string {
     if (displayFormat === 'input') {
