70d5c247c8a3796b07dd2a5ecb66bfa562a661f1
omunizb <oriolmuniz@gmail.com>
2020-11-16 21:36:37 +0100

    get stats graph data from server

--- ClientApp/src/app/stats/stats-graph/stats-graph.component.ts
+++ ClientApp/src/app/stats/stats-graph/stats-graph.component.ts
@@ -11,8 +11,8 @@ import { StatsService } from '../stats.service';
 })
 export class StatsGraphComponent implements OnInit {
   public lineChartData: ChartDataSets[] = [
-    { data: [2800, 4800, 4000, 2900, 8600, 9000 /*, 2700*/], label: 'units sold per month' },
-    { data: [18000, 48000, 43000, 19000, 100000, 85000 /*, 27000*/], label: 'monthly revenues (USD)', yAxisID: 'y-axis-1' }
+    { data: this.getUnits()/*[2800, 4800, 4000, 2900, 8600, 9000, 2700]*/, label: 'units sold per month' },
+    { data: this.getRevenue()/*[18000, 48000, 43000, 19000, 100000, 85000, 27000]*/, label: 'monthly revenues (USD)', yAxisID: 'y-axis-1' }
   ];
   public lineChartLabels: Label[] = this.getLabels();//['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: (ChartOptions) = {
@@ -44,11 +44,17 @@ export class StatsGraphComponent implements OnInit {
           }
         }
       ]
+    },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
     }
   };
   public lineChartColors: Color[] = [
     { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
+      backgroundColor: 'rgba(148,159,177,0.8)',
       borderColor: 'rgba(148,159,177,1)',
       pointBackgroundColor: 'rgba(148,159,177,1)',
       pointBorderColor: '#fff',
@@ -56,7 +62,7 @@ export class StatsGraphComponent implements OnInit {
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
     { // deep-purple
-      backgroundColor: 'rgba(103,58,183,0.3)',
+      backgroundColor: 'rgba(103,58,183,0.8)',
       borderColor: 'rgba(103,58,183,1)',
       pointBackgroundColor: 'rgba(103,58,183,1)',
       pointBorderColor: '#fff',
@@ -65,7 +71,7 @@ export class StatsGraphComponent implements OnInit {
     }
   ];
   public lineChartLegend = true;
-  public lineChartType: ChartType = 'line';
+  public lineChartType: ChartType = 'bar';
   // public lineChartPlugins = [pluginAnnotations];
 
   @ViewChild(BaseChartDirective, { static: true }) chart: BaseChartDirective;
@@ -83,6 +89,20 @@ export class StatsGraphComponent implements OnInit {
     this.statsService.getMonthlySales()
     .subscribe(sales => sales.forEach(s => labels.push(s.label)));
     return labels;
+  }
+
+  getUnits(): number[] {
+    var units = [];
+    this.statsService.getMonthlySales()
+    .subscribe(sales => sales.forEach(s => units.push(s.units)));
+    return units;
+  }
+
+  getRevenue(): number[] {
+    var revenue = [];
+    this.statsService.getMonthlySales()
+    .subscribe(sales => sales.forEach(s => revenue.push(s.revenue)));
+    return revenue;
   }
 
   // events
